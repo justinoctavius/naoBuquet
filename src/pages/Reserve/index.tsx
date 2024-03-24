7bd14f69329b1c2cb1b85d04257f74027cc9786c
justinoctavius <justinoctavio2001@gmail.com>
2024-03-23 22:44:18 -0400

    avances

--- src/pages/Reserve/index.tsx
+++ src/pages/Reserve/index.tsx
@@ -4,20 +4,55 @@ import { useReserve } from './hook';
 import { Calendar } from '../../components/molecules/Calendar';
 import { Box, Button, TextField, Typography } from '@mui/material';
 import { useMemo, useState } from 'react';
+import { EMAIL_REGEX } from '../../constants/regex';
 
 export const ReservePage = () => {
   const { id } = useParams();
   const { service } = useReserve({ id: id || '' });
 
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
+  const [emailHelperText, setEmailHelperText] = useState('');
+  const [fields, setFields] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+  const [helperText, setHelperText] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+
   const [reserveId, setReserveId] = useState<string | null>('');
 
   const isFormValid = useMemo(
-    () => firstName && lastName && email && reserveId,
-    [firstName, lastName, email, reserveId]
+    () => fields.firstName && fields.lastName && fields.email && reserveId,
+    [fields, reserveId]
   );
+
+  const validateEmail = () => {
+    if (!fields.email.match(EMAIL_REGEX)) {
+      setHelperText({ ...helperText, email: 'Correo inválido' });
+    } else {
+      setHelperText({ ...helperText, email: '' });
+    }
+  };
+
+  const validateFields = (fieldName: string) => {
+    const field = fields[fieldName];
+
+    if (!field) {
+      setHelperText({ ...helperText, [fieldName]: 'Campo requerido' });
+    }
+
+    if (fieldName === 'email') {
+      validateEmail();
+    }
+  };
+
+  const handleOnFieldChange = (field: string, value: string) => {
+    setFields({ ...fields, [field]: value });
+    setHelperText({ ...helperText, [field]: '' });
+  };
 
   return (
     <FormScreenTemplate>
@@ -36,16 +71,22 @@ export const ReservePage = () => {
           justifyContent={'center'}
         >
           <Typography variant="h6">Ingresa tus datos: </Typography>
-          <Box display={'flex'} flexDirection={'column'} gap={1}>
-            <Box display={'flex'} gap={1}>
+          <Box display={'flex'} flexDirection={'column'} gap={2}>
+            <Box display={'flex'} gap={2}>
               <TextField
                 id="filled-name"
                 label="Nombres"
                 type="email"
                 variant="outlined"
                 fullWidth
-                onChange={(e) => setFirstName(e.target.value)}
-                value={firstName}
+                onChange={(e) =>
+                  handleOnFieldChange('firstName', e.target.value)
+                }
+                value={fields.firstName}
+                onBlur={() => validateFields('firstName')}
+                helperText={helperText.firstName}
+                error={!!helperText.firstName}
+                required
               />
               <TextField
                 id="filled-lastname"
@@ -53,8 +94,14 @@ export const ReservePage = () => {
                 type="email"
                 variant="outlined"
                 fullWidth
-                onChange={(e) => setLastName(e.target.value)}
-                value={lastName}
+                onChange={(e) =>
+                  handleOnFieldChange('lastName', e.target.value)
+                }
+                value={fields.lastName}
+                onBlur={() => validateFields('lastName')}
+                helperText={helperText.lastName}
+                error={!!helperText.lastName}
+                required
               />
             </Box>
             <TextField
@@ -63,8 +110,12 @@ export const ReservePage = () => {
               type="email"
               variant="outlined"
               fullWidth
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
+              onChange={(e) => handleOnFieldChange('email', e.target.value)}
+              value={fields.email}
+              required
+              helperText={helperText.email}
+              error={!!helperText.email}
+              onBlur={() => validateFields('email')}
             />
           </Box>
           <Button variant="contained" fullWidth disabled={!isFormValid}>
