dc7aa8bbf3a2c396e2bfa53328317857168de28b
justinoctavius <justinoctavio2001@gmail.com>
2024-03-24 18:28:04 -0400

    avances

--- src/components/organisms/SideNavigator/index.tsx
+++ src/components/organisms/SideNavigator/index.tsx
@@ -1,10 +1,19 @@
 import React, { cloneElement } from 'react';
-import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import {
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import { NavLink, useLocation } from 'react-router-dom';
 import { colors } from '../../../constants/theme/colors';
 import { routes } from '../../../constants/routes';
+import { useAuth } from '../../../context/auth';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const options = [
   {
@@ -22,27 +31,43 @@ const options = [
 const SideNavigator: React.FC = () => {
   const location = useLocation();
 
+  const { logout } = useAuth();
+
   return (
-    <List>
-      {options.map(({ label, icon, path }, index) => (
-        <NavLink
-          to={path}
-          style={{ textDecoration: 'none', color: 'inherit' }}
-          key={index}
-        >
-          <ListItem
-            sx={{
-              bgcolor: location.pathname.includes(path)
-                ? colors.gray
-                : 'transparent',
-            }}
+    <Box>
+      <List>
+        {options.map(({ label, icon, path }, index) => (
+          <NavLink
+            to={path}
+            style={{ textDecoration: 'none', color: 'inherit' }}
+            key={index}
           >
-            <ListItemIcon>{cloneElement(icon)}</ListItemIcon>
-            <ListItemText primary={label} />
-          </ListItem>
-        </NavLink>
-      ))}
-    </List>
+            <ListItem
+              sx={{
+                bgcolor: location.pathname.includes(path)
+                  ? colors.gray
+                  : 'transparent',
+              }}
+            >
+              <ListItemIcon>{cloneElement(icon)}</ListItemIcon>
+              <ListItemText primary={label} />
+            </ListItem>
+          </NavLink>
+        ))}
+      </List>
+      <Box
+        position={'absolute'}
+        bottom={2}
+        display={'flex'}
+        padding={1}
+        width={'100%'}
+        justifyContent={'center'}
+      >
+        <Button variant="text" color="error" onClick={logout}>
+          <LogoutIcon fontSize={'small'} sx={{ mr: 1 }} /> Cerrar sesión
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
